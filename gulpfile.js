201d2346196d2e999968f989eebf493df74fc025
Lorenz Schimik <losc13@gmail.com>
2015-08-26 02:36:05 +0200

    website redesign

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+//load all our gulp plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    browserSync = require('browser-sync').create(),
+    del = require('del');
+
+//Compile sass files, minify & add prefixes
+gulp.task('styles', function() {
+  return sass('src/styles/style.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('css/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+//concat and minify JavaScript
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('static/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('static/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+//combine all 3 tasks
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+//watch our files and perform necessary tasks
+gulp.task('watch', function() {
+
+    //set up browser-sync
+    browserSync.init({
+            notify:false,
+            server: {baseDir: "./"}
+        });
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+  //browser reload
+  gulp.watch(['css/**']).on("change", browserSync.reload);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
